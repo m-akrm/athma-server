77bb83abc5216c429d9fd6fc2bc848043419b719
mahmoud akrm mahmoud mohamed <113924991+m-akrm@users.noreply.github.com>
2023-12-05 02:19:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,31 @@ from fastapi.responses import JSONResponse
 import librosa
 import tensorflow as tf
 import numpy as np
+from scipy.signal import resample
 
 
 model=tf.keras.models.load_model('diagnosis_GRU_CNN_1.h5')
 classes = ["COPD" ,"Healthy", "Pneumoina", "URTI", "Healthy"]
 
-def predict_class(audio_file_path, gru_model=model, classes=classes, features=52):
+def resample_audio(audio_data, original_sample_rate, target_sample_rate):
+    # Calculate the resampling factor
+    resampling_factor = target_sample_rate / original_sample_rate
+
+    # Calculate the new length of the resampled signal
+    new_length = int(len(audio_data) * resampling_factor)
+
+    # Resample the audio data
+    resampled_data = resample(audio_data, new_length)
+
+    return resampled_data
+
+def predict_class(data ,features=52):
+    audio_data = np.array(data, dtype=np.int8)
+    audio_float = librosa.util.buf_to_float(audio_data, dtype=np.float32)
+
     val = []
-    data_x, sampling_rate = librosa.load(audio_file_path , res_type='kaiser_fast')
-    mfccs = np.mean(librosa.feature.mfcc(y=data_x, sr=sampling_rate, n_mfcc=features).T, axis=0)
+
+    mfccs = np.mean(librosa.feature.mfcc(y=audio_float, sr=1000, n_mfcc=features).T, axis=0)
     val.append(mfccs)
     val = np.expand_dims(val, axis=1)
     prediction = classes[np.argmax(gru_model.predict(val))]
@@ -30,17 +46,13 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.post("/upload/")
-async def upload_sound_file(file: UploadFile):
-    if file.content_type.startswith("audio/"):
-        # Process the uploaded audio file with librosa
-        # For example, get the duration of the audio
-        prediction=predict_class(file.file)
-        return {'prediction': prediction}
-    return {'prediction': "Invalid file format"} 
 
 @app.post("/process_data")
 async def process_data(data: List[int]):
     # Your processing logic here
-    result = {"received_data": data[0]}
+    mapped_values = list(map(lambda x: int(x * 255 // 4096), data))
+
+    resampled_data_1000hz = resample_audio(mapped_values, 100, 1000)
+    prediction=predict_class(resampled_data_1000hz)
+    result = {"received_data": prediction}
     return result
